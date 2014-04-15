c28c098811638de2890a2874f38215b6a5adf89c
Philip Horger <campadrenalin@gmail.com>
2014-04-14 18:22:01 -0700

    Record.ToDns implemented

--- model/record.go
+++ model/record.go
@@ -1,14 +1,58 @@
 package model
 
+import (
+	"errors"
+	"fmt"
+	"github.com/miekg/dns"
+)
+
 type Record struct {
 	DomainName string      `json:"domain_name"`
+	Rttl       int         `json:"rttl"`
+	Rclass     string      `json:"rclass"`
 	Rtype      string      `json:"rtype"`
 	Rdata      interface{} `json:"rdata"`
 }
 
 // Fill in data that was left implied via defaults
-func (sr *Record) Normalize() {
-	if sr.Rtype == "" {
-		sr.Rtype = "A"
+func (r *Record) Normalize() {
+	if r.Rtype == "" {
+		r.Rtype = "A"
 	}
+}
+
+func (r *Record) RdataString() (string, error) {
+	var rdata string
+	var ok bool
+	var err error
+
+	switch r.Rtype {
+	case "A":
+	case "MX":
+		rdata, ok = r.Rdata.(string)
+	default:
+		return rdata, errors.New("Unknown Rtype")
+	}
+
+	if !ok {
+		err = errors.New("Rdata was wrong type for Rtype")
+	}
+	return rdata, err
+}
+
+// Export the data as dns.RR
+func (r *Record) ToDns() (dns.RR, error) {
+	rdata, err := r.RdataString()
+	if err != nil {
+		return nil, err
+	}
+	rr_string := fmt.Sprintf(
+		"%s %d %s %s %s",
+		r.DomainName,
+		r.Rttl,
+		r.Rclass,
+		r.Rtype,
+		rdata,
+	)
+	return dns.NewRR(rr_string)
 }
---
+++ model/record_test.go
@@ -0,0 +1,46 @@
+package model
+
+import (
+	"github.com/miekg/dns"
+	"reflect"
+	"testing"
+)
+
+type ToDnsTest struct {
+	DjRecord        Record
+	ExtRecordString string
+}
+
+func (test *ToDnsTest) ExtRecord() (dns.RR, error) {
+	return dns.NewRR(test.ExtRecordString)
+}
+
+func Test_Record_ToDns(t *testing.T) {
+	tests := []ToDnsTest{
+		ToDnsTest{
+			Record{
+				DomainName: "host.name.",
+				Rttl:       4000,
+				Rtype:      "MX",
+				Rdata:      "10 9.9.9.9",
+			},
+			"host.name. 4000 IN MX 10 9.9.9.9",
+		},
+	}
+	for _, test := range tests {
+		expected, err := test.ExtRecord()
+		if err != nil {
+			t.Fatal(err)
+		}
+		test.DjRecord.Normalize()
+		record, err := test.DjRecord.ToDns()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(record, expected) {
+			t.Log(record)
+			t.Log(expected)
+			t.Fatal("record != expected")
+		}
+	}
+}
