97a6cca5be42cc4d631c32595a6a470e358b122e
Philip Horger <campadrenalin@gmail.com>
2014-05-07 10:49:54 -0700

    More descriptive errors when JSON parse fails

--- model/file.go
+++ model/file.go
@@ -2,8 +2,31 @@ package model
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 )
+
+func FindLine(text []byte, offset int64) (line int64, char int64) {
+	// TODO: more line ending support using bytes.Replace
+    var pos int64
+    line = 1
+    char = 1
+    if (offset >= int64(len(text))) {
+        line, char = -1, -1
+        return
+    }
+    for pos < offset {
+        if text[pos] == '\n' {
+            line++
+            char = 1
+        } else {
+            char++
+        }
+
+        pos++
+    }
+	return
+}
 
 func GetJSONFromFile(filename string) (interface{}, error) {
 	raw, err := ioutil.ReadFile(filename)
@@ -13,5 +36,11 @@ func GetJSONFromFile(filename string) (interface{}, error) {
 
 	var result interface{}
 	err = json.Unmarshal(raw, &result)
+	if err != nil {
+		if serr, ok := err.(*json.SyntaxError); ok {
+			line, col := FindLine(raw, serr.Offset)
+			err = fmt.Errorf("line %d, char %d: %s", line, col, err)
+		}
+	}
 	return result, err
 }
--- model/file_test.go
+++ model/file_test.go
@@ -2,6 +2,46 @@ package model
 
 import "testing"
 
+type flCheck struct {
+	ByteOffset int64
+	Line       int64
+	Char       int64
+	Msg        string
+}
+
+func TestFindLine(t *testing.T) {
+	file_string := []byte(`first line
+second line
+
+
+fifth line, after some blanks`)
+	checks := []flCheck{
+		flCheck{0, 1, 1, "Zero-offset has 1-indexed line/char"},
+		flCheck{5, 1, 6, "Middle of first line"},
+		flCheck{9, 1, 10, "End of first line"},
+		flCheck{10, 1, 11, "Newline after line 1"},
+		flCheck{11, 2, 1, "Start of line 2"},
+		flCheck{15, 2, 5, "Middle of second line"},
+		flCheck{21, 2, 11, "End of second line"},
+		flCheck{22, 2, 12, "Newline after line 2"},
+		flCheck{23, 3, 1, "Line 3"},
+		flCheck{24, 4, 1, "Line 4"},
+		flCheck{25, 5, 1, "Start of line 5"},
+		flCheck{48, 5, 24, "The letter 'b'"},
+		flCheck{53, 5, 29, "Last char ('s')"},
+		flCheck{54, -1, -1, "After last char"},
+		flCheck{154, -1, -1, "Long after last char"},
+	}
+    //t.Fatal("%v", file_string)
+	for _, check := range checks {
+		line, char := FindLine(file_string, check.ByteOffset)
+		if line != check.Line || char != check.Char {
+			t.Error(check)
+			t.Fatalf("Got line %d, char %d", line, char)
+		}
+	}
+}
+
 // Success case
 func TestGetJSONFromFile(t *testing.T) {
 	value, err := GetJSONFromFile("demo.json")
@@ -25,10 +65,23 @@ func TestGetJSONFromFile(t *testing.T) {
 	}
 }
 
-// Failure case
+// Failure cases
 func TestGetJSONFromFile_NoSuchFile(t *testing.T) {
 	_, err := GetJSONFromFile("nosuch.file")
 	if err == nil {
 		t.Fatal("GetJSONFromFile should fail on missing file")
 	}
+}
+
+func TestGetJSONFromFile_BadJSON(t *testing.T) {
+	_, err := GetJSONFromFile("broken.json")
+	if err == nil {
+		t.Fatal("GetJSONFromFile should fail on ill-formed JSON")
+	}
+    err_expected := `line 3, char 2: invalid character '}' looking for beginning of object key string`
+    err_got := err.Error()
+    if err_got != err_expected {
+        t.Errorf("Expected: '%s'", err_expected)
+        t.Errorf("Got: '%s'", err_got)
+    }
 }
