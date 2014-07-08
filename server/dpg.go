54524deb2d8be6ee9b6c84007b2d4161ad7463ff
Philip Horger <campadrenalin@gmail.com>
2014-07-07 23:08:47 -0700

    #3 Fix DPG.getRouterUrl

--- server/dpg.go
+++ server/dpg.go
@@ -1,11 +1,9 @@
 package server
 
-/*
 import (
-	"fmt"
 	"net/url"
+	"strings"
 )
-*/
 
 // Retrieves DEJE documents, and uses their content
 type DejePageGetter struct {
@@ -15,8 +13,27 @@ func NewDejePageGetter() DejePageGetter {
 	return DejePageGetter{}
 }
 
-func (pg DejePageGetter) getRouterUrl(string) string {
-	return "Not a real value"
+func (pg DejePageGetter) getRouterUrl(deje_url string) (string, error) {
+	// Handle schemeless URIs
+	valid_prefixes := []string{"http://", "https://", "ws://", "//"}
+	has_valid_prefix := false
+	for _, prefix := range valid_prefixes {
+		if strings.HasPrefix(deje_url, prefix) {
+			has_valid_prefix = true
+		}
+	}
+	if !has_valid_prefix {
+		deje_url = "ws://" + deje_url
+	}
+
+	// Parse and fix up
+	url_obj, err := url.Parse(deje_url)
+	if err != nil {
+		return "", err
+	}
+	url_obj.Scheme = "ws"
+	url_obj.Path = "/ws"
+	return url_obj.String(), nil
 }
 
 func (dpg DejePageGetter) GetPage(urlstr string, ab Aborter) (Page, error) {
--- server/dpg_test.go
+++ server/dpg_test.go
@@ -12,12 +12,22 @@ func TestDejePageGetter_getRouterUrl(t *testing.T) {
 	tests := []UrlTest{
 		UrlTest{"http://foo/bar", "ws://foo/ws"},
 		UrlTest{"http://foo:8080", "ws://foo:8080/ws"},
+		UrlTest{"foo.bar.baz", "ws://foo.bar.baz/ws"},
 		UrlTest{"foo.bar.baz:8080", "ws://foo.bar.baz:8080/ws"},
+		UrlTest{"//foo.bar.baz:8080", "ws://foo.bar.baz:8080/ws"},
+		UrlTest{"%", "<error>: parse ws://%: hexadecimal escape in host"},
 	}
 	for _, test := range tests {
-		got := pg.getRouterUrl(test.Input)
+		got, err := pg.getRouterUrl(test.Input)
+		if err != nil {
+			got = "<error>: " + err.Error()
+		}
 		if got != test.Output {
-			t.Fatalf("Bad result:\ngot: '%s'\nexp: '%s'", got, test.Output)
+			t.Errorf("Bad result:\ninput: %s\ngot: '%s'\nexp: '%s'",
+				test.Input,
+				got,
+				test.Output,
+			)
 		}
 	}
 }
