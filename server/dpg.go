7568bcdefaa96fe625ac259e977475418a10cc63
Philip Horger <campadrenalin@gmail.com>
2014-07-24 22:57:54 -0700

    Close #10: Remove redundant functionality

--- server/dpg.go
+++ server/dpg.go
@@ -4,8 +4,6 @@ import (
 	"errors"
 	"io"
 	"log"
-	"net/url"
-	"strings"
 
 	deje "github.com/DJDNS/go-deje"
 	deje_doc "github.com/DJDNS/go-deje/document"
@@ -47,63 +45,15 @@ func NewDejePageGetter(w io.Writer) DejePageGetter {
 	}
 }
 
-// Handle schemeless URIs
-func (pg DejePageGetter) unbareUrl(deje_url string) string {
-	valid_prefixes := []string{"http://", "https://", "ws://", "deje://", "//"}
-	has_valid_prefix := false
-	for _, prefix := range valid_prefixes {
-		if strings.HasPrefix(deje_url, prefix) {
-			has_valid_prefix = true
-		}
-	}
-	if !has_valid_prefix {
-		deje_url = "ws://" + deje_url
-	}
-	return deje_url
-}
-
-func (pg DejePageGetter) getRouterUrl(deje_url string) (string, error) {
-	url_obj, err := url.Parse(pg.unbareUrl(deje_url))
-	if err != nil {
-		return "", err
-	}
-	url_obj.Scheme = "ws"
-	url_obj.Path = "/ws"
-	return url_obj.String(), nil
-}
-
-func (pg DejePageGetter) getTopic(deje_url string) (string, error) {
-	url_obj, err := url.Parse(pg.unbareUrl(deje_url))
-	if err != nil {
-		return "", err
-	}
-	url_obj.Scheme = "deje"
-	if url_obj.Path == "" {
-		url_obj.Path = "/"
-	}
-	return url_obj.String(), nil
-}
-
 func (pg DejePageGetter) getDoc(deje_url string) (*deje_doc.Document, error) {
 	dcd, ok := pg.clients[deje_url]
 	if !ok {
-		ws_url, err := pg.getRouterUrl(deje_url)
-		if err != nil {
-			return nil, err
-		}
-
-		topic, err := pg.getTopic(deje_url)
-		if err != nil {
-			return nil, err
-		}
-
 		var logger *log.Logger
 		if pg.writer != nil {
 			logger = log.New(pg.writer, "client '"+deje_url+"': ", 0)
 		}
 
-		client := deje.NewSimpleClient(topic, logger)
-		err = client.Connect(ws_url)
+		client, err := deje.Open(deje_url, logger, nil)
 		if err != nil {
 			return nil, err
 		}
--- server/dpg_test.go
+++ server/dpg_test.go
@@ -16,70 +16,20 @@ type UrlTest struct {
 	Output string
 }
 
-func TestDejePageGetter_getRouterUrl(t *testing.T) {
-	pg := NewDejePageGetter(nil)
-	tests := []UrlTest{
-		UrlTest{"http://foo/bar", "ws://foo/ws"},
-		UrlTest{"http://foo:8080", "ws://foo:8080/ws"},
-		UrlTest{"foo.bar.baz", "ws://foo.bar.baz/ws"},
-		UrlTest{"foo.bar.baz:8080", "ws://foo.bar.baz:8080/ws"},
-		UrlTest{"//foo.bar.baz:8080", "ws://foo.bar.baz:8080/ws"},
-		UrlTest{"deje://foo.bar.baz:8080", "ws://foo.bar.baz:8080/ws"},
-		UrlTest{"%", "<error>: parse ws://%: hexadecimal escape in host"},
-	}
-	for _, test := range tests {
-		got, err := pg.getRouterUrl(test.Input)
-		if err != nil {
-			got = "<error>: " + err.Error()
-		}
-		if got != test.Output {
-			t.Errorf("Bad result:\ninput: %s\ngot: '%s'\nexp: '%s'",
-				test.Input,
-				got,
-				test.Output,
-			)
-		}
-	}
-}
-
-func TestDejePageGetter_getTopic(t *testing.T) {
-	pg := NewDejePageGetter(nil)
-	tests := []UrlTest{
-		UrlTest{"http://foo/bar", "deje://foo/bar"},
-		UrlTest{"http://foo:8080", "deje://foo:8080/"},
-		UrlTest{"foo.bar.baz", "deje://foo.bar.baz/"},
-		UrlTest{"foo.bar.baz:8080", "deje://foo.bar.baz:8080/"},
-		UrlTest{"//foo.bar.baz:8080", "deje://foo.bar.baz:8080/"},
-		UrlTest{"deje://foo.bar.baz:8080", "deje://foo.bar.baz:8080/"},
-		UrlTest{"%", "<error>: parse ws://%: hexadecimal escape in host"},
-	}
-	for _, test := range tests {
-		got, err := pg.getTopic(test.Input)
-		if err != nil {
-			got = "<error>: " + err.Error()
-		}
-		if got != test.Output {
-			t.Errorf("Bad result:\ninput: %s\ngot: '%s'\nexp: '%s'",
-				test.Input,
-				got,
-				test.Output,
-			)
-		}
-	}
-}
-
 func setup_deje_env(t *testing.T) (string, string, func(), *deje.SimpleClient, deje.Client) {
 	tp := turnpike.NewServer()
 	router := httptest.NewServer(tp.Handler)
-	router_url := strings.Replace(router.URL, "http", "ws", 1)
-	topic := strings.Replace(router.URL, "http", "deje", 1) + "/"
+	deje_url := strings.Replace(router.URL, "http", "deje", 1) + "/"
+	router_url, topic, err := deje.GetRouterAndTopic(deje_url)
+	if err != nil {
+		t.Fatal(err)
+	}
 	closer := func() {
 		router.CloseClientConnections()
 		router.Close()
 	}
 
-	clever := deje.NewSimpleClient(topic, nil)
-	err := clever.Connect(router_url)
+	clever, err := deje.Open(deje_url, nil, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -96,14 +46,14 @@ func setup_deje_env(t *testing.T) (string, string, func(), *deje.SimpleClient, d
 
 func TestDejePageGetter_getDoc(t *testing.T) {
 	pg := NewDejePageGetter(nil)
-	url, _, closer, _, _ := setup_deje_env(t)
+	_, topic, closer, _, _ := setup_deje_env(t)
 	defer closer()
 
-	doc, err := pg.getDoc(url)
+	doc, err := pg.getDoc(topic)
 	if err != nil {
 		t.Fatal(err)
 	}
-	doc2, err := pg.getDoc(url)
+	doc2, err := pg.getDoc(topic)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -116,48 +66,41 @@ func TestDejePageGetter_getDoc(t *testing.T) {
 func TestDejePageGetter_getDoc_logging(t *testing.T) {
 	buf := new(bytes.Buffer)
 	pg := NewDejePageGetter(buf)
-	url, topic, closer, _, dumb := setup_deje_env(t)
+	_, topic, closer, _, dumb := setup_deje_env(t)
 	defer closer()
 
-	testLogging := func(deje_url string) {
-		t.Log(deje_url)
+	// Use fresh log, so hanger-on client has no access
+	buf = new(bytes.Buffer)
+	pg.writer = buf
 
-		// Use fresh log, so hanger-on client has no access
-		buf = new(bytes.Buffer)
-		pg.writer = buf
+	// Create client, wait for it to connect
+	_, err := pg.getDoc(topic)
+	if err != nil {
+		t.Fatal(err)
+	}
+	<-time.After(50 * time.Millisecond)
 
-		// Create client, wait for it to connect
-		_, err := pg.getDoc(deje_url)
-		if err != nil {
-			t.Fatal(err)
-		}
-		<-time.After(50 * time.Millisecond)
+	// Put something in the log, and wait for it to broadcast
+	if err = dumb.Publish("foo"); err != nil {
+		t.Fatal(err)
+	}
+	<-time.After(50 * time.Millisecond)
 
-		// Put something in the log, and wait for it to broadcast
-		if err = dumb.Publish("foo"); err != nil {
-			t.Fatal(err)
-		}
-		<-time.After(50 * time.Millisecond)
-
-		expected_log := "client '" + deje_url + "': Non-{} message\n"
-		got_log := buf.String()
-		if got_log != expected_log {
-			t.Fatalf("Log content was wrong.\nexp: '%s'\ngot: '%s'", expected_log, got_log)
-		}
+	expected_log := "client '" + topic + "': Non-{} message\n"
+	got_log := buf.String()
+	if got_log != expected_log {
+		t.Fatalf("Log content was wrong.\nexp: '%s'\ngot: '%s'", expected_log, got_log)
 	}
 
-	// Ensure that both are parseable
-	testLogging(url)
-	testLogging(topic)
 }
 
 func TestDejePageGetter_GetPage_Abort(t *testing.T) {
 	pg := NewDejePageGetter(nil)
-	url, _, closer, _, _ := setup_deje_env(t)
+	_, topic, closer, _, _ := setup_deje_env(t)
 	defer closer()
 
 	ab := time.After(0)
-	_, err := pg.GetPage(url, ab)
+	_, err := pg.GetPage(topic, ab)
 	if err == nil {
 		t.Fatal("Should have failed due to timeout")
 	}
@@ -172,7 +115,7 @@ func TestDejePageGetter_GetPage_Abort(t *testing.T) {
 func TestDejePageGetter_GetPage(t *testing.T) {
 	buf := new(bytes.Buffer)
 	pg := NewDejePageGetter(buf)
-	url, _, closer, clever, _ := setup_deje_env(t)
+	_, topic, closer, clever, _ := setup_deje_env(t)
 	defer closer()
 
 	// Set up event
@@ -188,7 +131,7 @@ func TestDejePageGetter_GetPage(t *testing.T) {
 	clever.Promote(event)
 
 	ab := time.After(50 * time.Millisecond)
-	page, err := pg.GetPage(url, ab)
+	page, err := pg.GetPage(topic, ab)
 	if err != nil {
 		t.Fatal(err)
 	}
