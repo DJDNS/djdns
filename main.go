50a9518ff0084ccd8d04618b00aae4d9f6a47820
Philip Horger <campadrenalin@gmail.com>
2014-09-01 22:53:00 -0700

    Close #14 Add --help and --root options

--- main.go
+++ main.go
@@ -2,28 +2,31 @@ package main
 
 //import "flag"
 import (
+	"flag"
 	"log"
 	"os"
 
 	"github.com/DJDNS/djdns/server"
 )
 
+var root_alias = flag.String("root", "deje://localhost:8080/root", "Target URL to serve as <ROOT>")
+
 func main() {
-	root_alias := "deje://localhost:8080/root"
+	flag.Parse()
 	addr := "127.0.0.1:9953"
 
 	logger := log.New(os.Stderr, "djdns: ", 0)
 
 	spgc := server.NewStandardPGConfig(os.Stderr)
 	spgc.Alias.Aliases = map[string]string{
-		"<ROOT>": root_alias,
+		"<ROOT>": *root_alias,
 	}
 
 	s := server.NewServer(spgc.Alias)
 	s.Logger = logger
 
 	logger.Printf("Starting server on %s", addr)
-	logger.Printf("<ROOT> is '%s'", root_alias)
+	logger.Printf("<ROOT> is '%s'", *root_alias)
 	err := s.Run(addr)
 	if err != nil {
 		log.Fatal(err)
