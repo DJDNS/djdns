627f18a852e6a1ccb5f2ff86a3750389a159cfcd
Philip Horger <campadrenalin@gmail.com>
2014-09-12 23:09:44 -0700

    Close #15: Send log entries over the wire to root page

--- main.go
+++ main.go
@@ -3,19 +3,39 @@ package main
 //import "flag"
 import (
 	"flag"
+	"io"
 	"log"
 	"os"
 
 	"github.com/DJDNS/djdns/server"
+	"github.com/DJDNS/go-deje"
 )
 
 var root_alias = flag.String("root", "deje://localhost:8080/root", "Target URL to serve as <ROOT>")
+
+type PeerWriter struct {
+	RealWriter io.Writer
+	Client     *deje.SimpleClient
+}
+
+func (pl PeerWriter) Write(p []byte) (n int, err error) {
+	pl.Client.Publish(map[string]interface{}{
+		"type":  "log",
+		"value": string(p),
+	})
+	return pl.RealWriter.Write(p)
+}
 
 func main() {
 	flag.Parse()
 	addr := "127.0.0.1:9953"
 
-	logger := log.New(os.Stderr, "djdns: ", 0)
+	peer_writer_client, err := deje.Open(*root_alias, nil, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log_writer := PeerWriter{os.Stderr, peer_writer_client}
+	logger := log.New(log_writer, "djdns: ", 0)
 
 	spgc := server.NewStandardPGConfig(os.Stderr)
 	spgc.Alias.Aliases = map[string]string{
@@ -27,7 +47,7 @@ func main() {
 
 	logger.Printf("Starting server on %s", addr)
 	logger.Printf("<ROOT> is '%s'", *root_alias)
-	err := s.Run(addr)
+	err = s.Run(addr)
 	if err != nil {
 		log.Fatal(err)
 	}
